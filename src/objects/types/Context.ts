8e88c1c4e9eddf4be6dd82aa887d704096bb1a1f
ana-max <anastasiamaximovna17@mail.ru>
2021-03-18 21:09:09 +0500

    Добавление нового поля для Context

--- src/objects/types/Context.ts
+++ src/objects/types/Context.ts
@@ -1,10 +1,13 @@
 import { API } from "@umk-stat/statistic-server-database";
+import { ClientAPI } from "@umk-stat/statistic-server-client-database";
 import { Logger } from "winston";
 import DataLoader from "dataloader";
 
 export class Context {
 
     readonly databaseApi: API;
+
+    readonly clientDatabaseApi: ClientAPI;
 
     readonly infoLogger: Logger;
 
@@ -16,12 +19,14 @@ export class Context {
 
     constructor(
         databaseAPi: API,
+        clientDatabaseApi: ClientAPI,
         infoLogger: Logger,
         errorLogger: Logger,
         graphQLObjectMap: Map<string, string>,
     ) {
 
         this.databaseApi = databaseAPi;
+        this.clientDatabaseApi = clientDatabaseApi;
         this.infoLogger = infoLogger;
         this.errorLogger = errorLogger;
         this.graphQLObjectMap = graphQLObjectMap;
